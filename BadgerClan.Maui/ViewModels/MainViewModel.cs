3f94bf6a3d8900c306fa0c7ffaa0b89e826f9299
JadedRain <darkenedninja450@gmail.com>
2025-02-05 13:51:39 -0700

    Added server changing

--- BadgerClan.Maui/ViewModels/MainViewModel.cs
+++ BadgerClan.Maui/ViewModels/MainViewModel.cs
@@ -13,8 +13,12 @@ namespace BadgerClan.Maui.ViewModels;
 public partial class MainViewModel : ObservableObject
 {
     private HttpClient _httpClient;
+    private string _uri;
     [ObservableProperty]
     private int _id;
+    [ObservableProperty]
+    private string _serverName;
+
 
     [RelayCommand]
     private async Task ChangeStrategy(string id)
@@ -23,14 +27,39 @@ public partial class MainViewModel : ObservableObject
         OnPropertyChanged(nameof(Id));
         await SendStrategyChange();
     }
+
+    [RelayCommand]
+    private void ChangeServer(string servername)
+    {
+        ServerName = servername;
+        SetUri(servername);
+        OnPropertyChanged(nameof(ServerName));
+    }
     
     private async Task SendStrategyChange()
     {
-        await _httpClient.PostAsJsonAsync($"https://badgerclanloganbot1-hqh7htb3gkf2gbes.westus-01.azurewebsites.net/setmove/{Id}", Id);
+        await _httpClient.PostAsJsonAsync($"{_uri}/setmove/{Id}", Id);
+    }
+    private void SetUri(string servername)
+    {
+        switch (servername)
+        {
+            case "Server 1":
+                _uri = "https://badgerclanloganbot1-hqh7htb3gkf2gbes.westus-01.azurewebsites.net";
+                break;
+            case "Server 2":
+                _uri = "";
+                break;
+            default:
+                _uri = "http://localhost:5217";
+                break;
+        }
     }
     public MainViewModel(HttpClient httpClient)
     {
         Id = 2;
+        ServerName = "Local";
+        _uri = "http://localhost:5217";
         _httpClient = httpClient;
     }
 }
