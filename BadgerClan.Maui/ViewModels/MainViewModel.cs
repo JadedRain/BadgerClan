afb9c8cf4b1ed2bb8d0d538d66a4cbdb57337a55
JadedRain <darkenedninja450@gmail.com>
2025-02-05 19:58:27 -0700

    Can swap between/controll multiple servers

--- BadgerClan.Maui/ViewModels/MainViewModel.cs
+++ BadgerClan.Maui/ViewModels/MainViewModel.cs
@@ -1,65 +1,61 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
+
 namespace BadgerClan.Maui.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
     private HttpClient _httpClient;
-    private string _uri;
+
     [ObservableProperty]
     private int _id;
     [ObservableProperty]
-    private string _serverName;
+    private ObservableCollection<Server> _servers;
 
 
     [RelayCommand]
     private async Task ChangeStrategy(string id)
     {
         Id = int.Parse(id);
+        foreach (var server in Servers.Where(i => i.IsChecked))
+        {
+            await SendStrategyChange(server.Uri);
+        }
         OnPropertyChanged(nameof(Id));
-        await SendStrategyChange();
-    }
-
-    [RelayCommand]
-    private async Task ChangeServer(string servername)
-    {
-        ServerName = servername;
-        await SetUri(servername);
-        OnPropertyChanged(nameof(ServerName));
     }
     
-    private async Task SendStrategyChange()
+    private async Task SendStrategyChange(string uri)
     {
-        await _httpClient.PostAsJsonAsync($"{_uri}/setmove/{Id}", Id);
-    }
-    private async Task SetUri(string servername)
-    {
-        switch (servername)
-        {
-            case "Server 1":
-                _uri = "https://badgerclanloganbot1-hqh7htb3gkf2gbes.westus-01.azurewebsites.net";
-                break;
-            case "Server 2":
-                _uri = "https://badgerclanloganbot2-g3bfaqgge7fqbtfp.canadacentral-01.azurewebsites.net";
-                break;
-            default:
-                _uri = "http://localhost:5217";
-                break;
-        }
+        await _httpClient.PostAsJsonAsync($"{uri}/setmove/{Id}", Id);
     }
     public MainViewModel(HttpClient httpClient)
     {
         Id = 2;
-        ServerName = "Local";
-        _uri = "http://localhost:5217";
         _httpClient = httpClient;
+        Servers = new()
+        {
+            new Server() {Name = "Local", Uri =  "http://localhost:5217", IsChecked = true},
+            new Server() {Name = "Server 1", Uri =  "https://badgerclanloganbot1-hqh7htb3gkf2gbes.westus-01.azurewebsites.net", IsChecked = false},
+            new Server() {Name = "Server 2", Uri =  "https://badgerclanloganbot2-g3bfaqgge7fqbtfp.canadacentral-01.azurewebsites.net", IsChecked = false}
+        };
+  
+
     }
+}
+
+public class Server
+{
+    public string Name { get; set; }
+    public string Uri { get; set;  }
+    public bool IsChecked { get; set; }
 }
